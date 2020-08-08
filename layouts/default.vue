cc268c596190d9c09a7cf8712d80cfbfa174cedd
Dries Bos <info@driesbos.com>
2020-08-08 20:56:26 +0200

    bg change on route check

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,35 @@ import TheNavigation from "~/components/TheNavigation.vue"
 export default {
   components: {
     "the-navigation": TheNavigation
+  },
+  data() {
+    return {
+      backgroundColor: 0
+    }
+  },
+  watch: {
+    $route() {
+      this.changeBackground()
+    }
+  },
+  mounted() {
+    this.changeBackground()
+  },
+  methods: {
+    changeBackground() {
+      console.log("ROUTE", this.$route)
+      if (this.$route.name === "index") {
+        document.body.style.backgroundColor = "#C0C9E5"
+      } else if (this.$route.name === "blog") {
+        document.body.style.backgroundColor = "#E4915D"
+      } else if (this.$route.path === "/samenwerken") {
+        document.body.style.backgroundColor = "#3D8B74"
+      } else if (this.$route.path === "/contact") {
+        document.body.style.backgroundColor = "#C0C9E5"
+      } else {
+        document.body.style.backgroundColor = "#C0C9E5"
+      }
+    }
   }
 }
 </script>
