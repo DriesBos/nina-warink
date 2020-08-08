dbccdfb83fd0e5531f92eca7ae67ca40d718800c
Dries Bos <info@driesbos.com>
2020-08-08 17:13:23 +0200

    added nav

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -4,7 +4,8 @@
       <ul>
         <nuxt-link to="/" tag="li">Home</nuxt-link>
         <nuxt-link to="/blog" tag="li">Blog</nuxt-link>
-        <nuxt-link to="/about" tag="li">About</nuxt-link>
+        <nuxt-link to="/samenwerken" tag="li">Samenwerken</nuxt-link>
+        <nuxt-link to="/contact" tag="li">Contact</nuxt-link>
       </ul>
     </nav>
   </header>
@@ -15,3 +16,27 @@ export default {
   name: "TheNavigation"
 }
 </script>
+
+<style lang="sass">
+header
+  position: fixed
+  top: 0
+  left: 0
+  width: 100%
+  z-index: 999
+  display: flex
+  justify-content: center
+  ul
+    display: flex
+    justify-content: flex-end
+    li
+      padding-top: 2em
+      margin-bottom: 2rem
+      margin-right: 1.25em
+      cursor: pointer
+    li:last-child
+      padding-right: 0
+  .nuxt-link-exact-active
+    text-decoration: none
+    border-bottom: 1px solid white
+</style>
