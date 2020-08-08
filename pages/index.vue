f704537b24683878222b7758ff906461d9401206
Dries Bos <info@driesbos.com>
2020-08-08 17:13:32 +0200

    init animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <section class="section section-Animation">
+      <div id="object"></div>
+      <svg
+        id="path"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        viewBox="0 0 1440 900"
+        d="M127.8,484.5c0,37.4-24.3,64.7-63.9,64.7C24.5,549.2,0,521.9,0,484.5c0-37.4,24.5-64.7,63.9-64.7
+            C103.5,419.8,127.8,447.1,127.8,484.5z M101.3,484.5c0-29-16.5-39.2-37.4-39.2c-20.8,0-37.4,10.2-37.4,39.2
+            c0,28.8,16.7,39.2,37.4,39.2C84.9,523.7,101.3,514.3,101.3,484.5z"
+      />
+    </section>
     <section class="section section-Landing">
       <div class="display">
         <h1>Nina Warink</h1>
@@ -50,6 +64,12 @@
 
 <script>
 import storyblokLivePreview from "@/mixins/storyblokLivePreview"
+import gsap from "gsap"
+// import $ from "jquery"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { MotionPathPlugin } from "gsap/MotionPathPlugin"
+
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin)
 
 export default {
   mixins: [storyblokLivePreview],
@@ -80,6 +100,33 @@ export default {
   data() {
     return {
       story: { content: {} }
+    }
+  },
+  mounted() {
+    this.pathAnimation()
+  },
+  methods: {
+    pathAnimation() {
+      // Center
+      gsap.set("#object", {
+        xPercent: -50,
+        yPercent: -50,
+        transformOrigin: "50% 50%"
+      })
+      // Animation
+      gsap.to("#object", {
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".section-Animation",
+          scrub: true
+        },
+        motionPath: {
+          path: "#path",
+          align: "#path",
+          alignOrigin: [0.5, 0.5],
+          autoRotate: true
+        }
+      })
     }
   }
 }
