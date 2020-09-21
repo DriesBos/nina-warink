058ea926d14ceee34c945a3f64f5352cdfdfde79
Dries Bos <driesbos@MBP-van-Dries.home>
2020-09-21 11:11:49 +0200

    added titleSmall component

---
+++ components/ItemTitleSmall.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="subSection subSection-TitleSmall titleSmall">
+    <markdown-item :input="blok.text" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: Object
+  },
+  mounted() {
+    // console.log("ITEM TITLE", this.blok)
+  }
+}
+</script>
