0ecf850122f7441b2ba4e20f29031c47170c3b2f
Dries Bos <driesbos@MBP-van-Dries.home>
2020-09-21 12:30:22 +0200

    mainLanding animation init

--- components/ItemMainlanding.vue
+++ components/ItemMainlanding.vue
@@ -1,9 +1,14 @@
 <template>
   <section
-    class="section subSection section-Mainlanding title"
+    class="section section-Mainlanding"
     :class="blok.title_bottom_padding"
   >
-    <h1>Nina Warink</h1>
+    <div class="logo">
+      <div v-html="require('~/assets/images/logo.svg?include')" />
+    </div>
+    <div id="heart-one" class="heart">
+      <div class="icon" v-html="require('~/assets/images/heart.svg?include')" />
+    </div>
   </section>
 </template>
 
@@ -25,25 +30,10 @@ export default {
   },
   methods: {
     pathAnimation() {
-      // Center
-      gsap.set("#object", {
-        xPercent: -50,
-        yPercent: -50,
-        transformOrigin: "50% 50%"
-      })
-      // Animation
-      gsap.to("#object", {
-        duration: 1,
-        scrollTrigger: {
-          trigger: ".section-Animation",
-          scrub: true
-        },
-        motionPath: {
-          path: "#path",
-          align: "#path",
-          alignOrigin: [0.5, 0.5],
-          autoRotate: true
-        }
+      gsap.to("#heart-one", {
+        duration: 10,
+        motionPath:
+          "M 100 300 Q 150 50 200 300 Q 250 550 300 300 Q 350 50 400 300 C 450 550 450 50 500 300 C 550 50 550 550 600 300 A 50 50 0 1 1 700 300"
       })
     }
   }
@@ -53,7 +43,23 @@ export default {
 <style lang="sass">
 .section
     &-Mainlanding
+        position: relative
         display: flex
         align-items: center
         min-height: 100vh
+        width: 100%
+        .logo
+            width: 100%
+            height: auto
+        .heart
+            position: fixed
+            left: 0
+            top: 0
+            right: 0
+            bottom: 0
+            svg
+                height: 39px
+                width: auto
+            // width: 100px
+            // height: 100px
 </style>
