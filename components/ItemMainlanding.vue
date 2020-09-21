afadcef657b01221148812f6c5ec81b813e8d8b1
Dries Bos <driesbos@MBP-van-Dries.home>
2020-09-21 11:58:02 +0200

    landing item added

---
+++ components/ItemMainlanding.vue
@@ -0,0 +1,59 @@
+<template>
+  <section
+    class="section subSection section-Mainlanding title"
+    :class="blok.title_bottom_padding"
+  >
+    <h1>Nina Warink</h1>
+  </section>
+</template>
+
+<script>
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { MotionPathPlugin } from "gsap/MotionPathPlugin"
+
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin)
+
+export default {
+  props: {
+    blok: Object
+  },
+  mounted() {
+    console.log("ITEM MAINLANDING", this.blok)
+    this.pathAnimation()
+    // console.log("PAGETYPE INDEX", this.story)
+  },
+  methods: {
+    pathAnimation() {
+      // Center
+      gsap.set("#object", {
+        xPercent: -50,
+        yPercent: -50,
+        transformOrigin: "50% 50%"
+      })
+      // Animation
+      gsap.to("#object", {
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".section-Animation",
+          scrub: true
+        },
+        motionPath: {
+          path: "#path",
+          align: "#path",
+          alignOrigin: [0.5, 0.5],
+          autoRotate: true
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.section
+    &-Mainlanding
+        display: flex
+        align-items: center
+        min-height: 100vh
+</style>
--- plugins/components.js
+++ plugins/components.js
@@ -3,6 +3,7 @@ import Page from "~/components/Page.vue"
 import Post from "~/components/Post.vue"
 import ContainerContent from "~/components/ContainerContent.vue"
 import ItemLanding from "~/components/ItemLanding.vue"
+import ItemMainlanding from "~/components/ItemMainlanding.vue"
 import ItemImages from "~/components/ItemImages.vue"
 import ItemTitle from "~/components/ItemTitle.vue"
 import ItemSubtitle from "~/components/ItemSubtitle.vue"
@@ -14,6 +15,7 @@ Vue.component("blok-post", Post)
 Vue.component("blok-container-content", ContainerContent)
 Vue.component("blok-item-images", ItemImages)
 Vue.component("blok-item-landing", ItemLanding)
+Vue.component("blok-item-mainlanding", ItemMainlanding)
 Vue.component("blok-item-title", ItemTitle)
 Vue.component("blok-item-subtitle", ItemSubtitle)
 Vue.component("blok-item-text-large", ItemTextLarge)
