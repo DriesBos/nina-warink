cad0b643f96e95c7a30ea600492a681a00d6986c
Dries Bos <driesbos@MBP-van-Dries.home>
2020-09-24 20:23:51 +0200

    added tittleSmall component

--- plugins/components.js
+++ plugins/components.js
@@ -6,6 +6,7 @@ import ItemLanding from "~/components/ItemLanding.vue"
 import ItemMainlanding from "~/components/ItemMainlanding.vue"
 import ItemImages from "~/components/ItemImages.vue"
 import ItemTitle from "~/components/ItemTitle.vue"
+import ItemTitleSmall from "~/components/ItemTitleSmall.vue"
 import ItemSubtitle from "~/components/ItemSubtitle.vue"
 import ItemTextLarge from "~/components/ItemTextLarge.vue"
 import ItemTextNormal from "~/components/ItemTextNormal.vue"
@@ -17,6 +18,7 @@ Vue.component("blok-item-images", ItemImages)
 Vue.component("blok-item-landing", ItemLanding)
 Vue.component("blok-item-mainlanding", ItemMainlanding)
 Vue.component("blok-item-title", ItemTitle)
+Vue.component("blok-item-title-small", ItemTitleSmall)
 Vue.component("blok-item-subtitle", ItemSubtitle)
 Vue.component("blok-item-text-large", ItemTextLarge)
 Vue.component("blok-item-text-normal", ItemTextNormal)
