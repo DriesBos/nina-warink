2281e619092985538e0a826282334effabbffc23
Dries Bos <driesbos@MBP-van-Dries.home>
2020-09-25 13:37:19 +0200

    error page with vuex data

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,26 @@
 <template>
-  <p>404 ERROR</p>
+  <div v-editable="error[0].content" class="page page-Error">
+    <component
+      :is="error[0].content.component | dashify"
+      v-if="error[0].content.component"
+      :key="error[0].content._uid"
+      :blok="error[0].content"
+    ></component>
+  </div>
 </template>
+
+<script>
+import { mapState } from "vuex"
+import storyblokLivePreview from "@/mixins/storyblokLivePreview"
+
+export default {
+  mixins: [storyblokLivePreview],
+  // props: ["error"],
+  layout: "default", // you can set a custom layout for the error page
+  computed: {
+    ...mapState({
+      error: state => state.error.list
+    })
+  }
+}
+</script>
