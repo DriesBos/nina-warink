c25eb3a3d628ebf79ca0c7ddcbcf8e2a915273bc
Dries Bos <driesbos@MacBook-Pro-van-Dries.local>
2020-09-28 12:07:47 +0200

    fix: error page data

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="page page-Error">
-    <!-- <section class="section subSection section-Landing title small">
-      <h1>Oops!</h1>
-    </section>
-    <section class="section section-Content">
-      <div class="subSection subSection-TextLarge textLarge">
-        <p>
-          Deze pagina bestaat niet. Bekijk mijn blog of ga terug naar de
-          homepage.
-        </p>
-      </div>
-    </section> -->
-    <p>oops</p>
+    <div id="pageComponent" class="section section-Wrapper">
+      <section class="section subSection section-Landing title small">
+        <h1>Oops!</h1>
+      </section>
+      <section class="section section-Content">
+        <div class="subSection subSection-TextLarge textLarge">
+          <div class="markdown">
+            <p>
+              Deze pagina bestaat niet. Bekijk mijn
+              <nuxt-link to="/blog">blog</nuxt-link> of ga terug naar de
+              <nuxt-link to="/">homepage</nuxt-link>.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  // layout: "default", // you can set a custom layout for the error page
+  layout: "default", // you can set a custom layout for the error page
   mounted() {
     console.log("ERROR PAGE")
   },
