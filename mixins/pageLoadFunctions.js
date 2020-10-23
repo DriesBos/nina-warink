b2ba0a2697d058ee33e16d5d9ba41c24863710fc
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-23 11:05:43 +0200

    fix: first el margin

--- mixins/pageLoadFunctions.js
+++ mixins/pageLoadFunctions.js
@@ -31,13 +31,11 @@ export default {
       }
     },
     setFirstSectionMargin() {
-      console.log("FIRED")
-      //   var wrapper = document.querySelector(".section-Wrapper")
-      //   console.log("FIRED", wrapper)
-      //   var section = wrapper.firstChild
-      //   var sectionHeight = section.offsetHeight / 2
-      //   var windowHeight = window.innerHeight / 2
-      //   section.style.marginTop = windowHeight - sectionHeight + "px"
+      var wrapper = document.querySelector(".section-Wrapper")
+      var section = wrapper.firstChild
+      var sectionHeight = section.offsetHeight / 2
+      var windowHeight = window.innerHeight / 2
+      section.style.marginTop = windowHeight - sectionHeight + "px"
     },
     changeBackground() {
       if (this.$route.name === "index") {
